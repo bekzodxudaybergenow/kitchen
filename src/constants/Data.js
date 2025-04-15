ed2cb69fbbda3f389ccbd83a7029c800a52e163e
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2025-04-15 23:09:08 +0500

    Update data file

--- src/constants/Data.js
+++ src/constants/Data.js
@@ -1,4 +1,17 @@
 import m1 from '../assets/m-1.png'
+import m2 from '../assets/m-2.png'
+import m3 from '../assets/m-3.png'
+import m4 from '../assets/m-4.png'
+
+import s1 from '../assets/s-1.png'
+import s2 from '../assets/s-2.png'
+import s3 from '../assets/s-3.png'
+import s4 from '../assets/s-4.png'
+
+import d1 from '../assets/d-1.png'
+import d2 from '../assets/d-2.png'
+import d3 from '../assets/d-3.png'
+import d4 from '../assets/d-4.png'
 
 export const employees = [
     {
@@ -73,171 +86,70 @@ export const meals = [
         price: 25_000
     },
     {
-        image: '/public/meals/m-2.png',
+        image: m2,
         name: 'Kabob',
         category: 'meal',
         price: 18_000
     },
     {
-        image: '/public/meals/m-3.png',
+        image: m3,
         name: 'Shorva',
         category: 'meal',
         price: 23_000
     },
     {
-        image: '/public/meals/m-4.png',
+        image: m4,
         name: 'Mastava',
         category: 'meal',
         price: 28_000
     },
     {
-        image: '/public/salads/s-1.png',
+        image: s1,
         name: 'Achichuk',
         category: 'salat',
         price: 10_000
     },
     {
-        image: '/public/salads/s-2.png',
+        image: s2,
         name: 'Suzma',
         category: 'salat',
         price: 8_000
     },
     {
-        image: '/public/salads/s-3.png',
+        image: s3,
         name: 'Svejiy',
         category: 'salat',
         price: 5_000
     },
     {
-        image: '/public/salads/s-4.png',
+        image: s4,
         name: 'Grekcha ',
         category: 'salat',
         price: 5_000
     },
     {
-        image: '/public/drinks/d-1.png',
+        image: d1,
         name: 'Pepsi 1.5l',
         category: 'drink',
         price: 12_000
     },
     {
-        image: '/public/drinks/d-2.png',
+        image: d2,
         name: 'Coola 1.5l',
         category: 'drink',
         price: 13_000
     },
     {
-        image: '/public/drinks/d-3.png',
+        image: d3,
         name: 'Fanta',
         category: 'drink',
         price: 10_000
     },
     {
-        image: '/public/drinks/d-4.png',
+        image: d4,
         name: 'Suv',
         category: 'drink',
         price: 5_000
     }
 
 ]
-// export const drinks = [
-//     {
-//         image: '',
-//         name: 'Pepsi 1.5l',
-//         category: 'drink',
-//         price: 15_000
-//     },
-//     {
-//         image: '',
-//         name: 'Coola 1.5l',
-//         category: 'drink',
-//         price: 15_000
-//     },
-//     {
-//         image: '',
-//         name: 'Fanta',
-//         category: 'drink',
-//         price: 15_000
-//     },
-//     {
-//         image: '',
-//         name: 'Choy',
-//         category: 'drink',
-//         price: 5_000
-//     }
-    // {
-    //     image: '/public/meals/m-5.png',
-    //     name: 'Xash',
-    //     category: 'meal',
-    //     price: 21_000
-    // },
-    // {
-    //     image: '/public/meals/m-6.png',
-    //     name: 'Norin',
-    //     category: 'meal',
-    //     price: 30_000
-    // },
-    // {
-    //     image: '/public/meals/m-7.png',
-    //     name: 'Norin Bifshteks',
-    //     category: 'meal',
-    //     price: 28_000
-    // },
-    // {
-    //     image: '/public/meals/m-8.png',
-    //     name: 'Qozon kabob',
-    //     category: 'meal',
-    //     price: 35_000
-    // }
-// ]
-// export const salads = [
-//     {
-//         image: '/public/salads/s-1.png',
-//         name: 'Achichuk',
-//         category: 'salat',
-//         price: 10_000
-//     },
-//     {
-//         image: '/public/salads/s-2.png',
-//         name: 'Suzma',
-//         category: 'salat',
-//         price: 8_000
-//     },
-//     {
-//         image: '/public/salads/s-3.png',
-//         name: 'Svejiy',
-//         category: 'salat',
-//         price: 5_000
-//     },
-//     {
-//         image: '/public/salads/s-4.png',
-//         name: 'Grekcha ',
-//         category: 'salat',
-//         price: 5_000
-//     },
-//     {
-//         image: '/public/salads/s-5.png',
-//         name: 'Oliviya',
-//         category: 'salat',
-//         price: 12_000
-//     },
-//     {
-//         image: '/public/salads/s-6.png',
-//         name: 'Tarnov',
-//         category: 'salat',
-//         price: 11_000
-//     },
-//     {
-//         image: '/public/salads/s-7.png',
-//         name: 'Damskiy',
-//         category: 'salat',
-//         price: 4_000
-//     },
-//     {
-//         image: '/public/salads/s-8.png',
-//         name: 'Yaponcha',
-//         category: 'salat',
-//         price: 14_000
-//     },
-
-// ]
